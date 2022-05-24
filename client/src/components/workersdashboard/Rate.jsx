f7556fc9b5db068e105af75ea935ff5a8f8f6f85
__francis <francis.coffie@amalitech.org>
2022-05-24 15:02:03 +0000

    rate

--- client/src/components/workersdashboard/Rate.jsx
+++ client/src/components/workersdashboard/Rate.jsx
@@ -3,5 +3,14 @@ import React, { useEffect, useState } from 'react';
 
 
 export default function Rate() {
-    
+    const { register } = useForm();
+    const [rank, setRank] = useState('');
+    const [salary, setSalary] = useState('');
+    const [bonus, setBonus] = useState('');
+    const [taxrelief, setTaxrelief] = useState('');
+    const [incometax, setIncometax] = useState('');
+    const [tierone, setTierone] = useState('');
+    const [tiertwo, setTiertwo] = useState('');
+    const [loandeduction, setLoandeduction] = useState('')
+
 }
