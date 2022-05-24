84cdc600057824e365fb2c7a49feb261c53f40cb
__francis <francis.coffie@amalitech.org>
2022-05-24 15:14:24 +0000

    import

---
+++ client/src/components/dashboard/LoanDashboard.jsx
@@ -0,0 +1,6 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import "./LoanDashboard.css"
+import Topbar from "../topbar/Topbar";
+import Sidebar from "../sidebar/Sidebar";
+import Footer from "../footer/Footer";
