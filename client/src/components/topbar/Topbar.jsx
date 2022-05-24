2c2094aae2c013386a4188e26c092368c3ec5968
__francis <francis.coffie@amalitech.org>
2022-05-24 16:11:13 +0000

    topbar wrapper

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,3 +1,13 @@
 import React from 'react';
 import "./Topbar.css";
 import PayrollImg from "../../assets/ipayroll.jpeg";
+
+
+export default function Topbar() {
+    return (
+        <div className="topbar">
+            <div className="topbarWrapper">
+                <div className="topleft"> 
+                    <img src={PayrollImg} alt="ipayroll" className="icon"/>
+                    <span className="logo">AMALITECH.ORG</span>
+             
