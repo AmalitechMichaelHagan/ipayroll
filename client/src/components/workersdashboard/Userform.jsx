be0403e66d7abcf2c1b719b501ebce7e9abeb9b9
__francis <francis.coffie@amalitech.org>
2022-05-24 17:12:23 +0000

    register user

--- client/src/components/workersdashboard/Userform.jsx
+++ client/src/components/workersdashboard/Userform.jsx
@@ -2,3 +2,11 @@ import "./Userform.css";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import axios from 'axios';
+
+
+export default function Userform() {
+    const { register } = useForm();
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
