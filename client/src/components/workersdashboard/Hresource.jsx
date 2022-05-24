081ce259d7394ded0ccbf5002190a2861a2e5d77
__francis <francis.coffie@amalitech.org>
2022-05-24 16:22:31 +0000

    endpoint

--- client/src/components/workersdashboard/Hresource.jsx
+++ client/src/components/workersdashboard/Hresource.jsx
@@ -5,3 +5,14 @@ import { useNavigate } from "react-router-dom";
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
 import Rate from "./Rate";
+
+
+export default function Hresource() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        axios.get(`http://localhost:9000/employees/all`)
+            .then(response => {
+                setAPIData(response.data);
+            })
+    }, [])
