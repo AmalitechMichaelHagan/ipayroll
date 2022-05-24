3af59d242000fa1cf82d87a95376ad2c3ad3eecf
Michael Hagan <91923744+AmalitechMichaelHagan@users.noreply.github.com>
2022-05-24 21:13:17 +0000

    mailOauth

--- backend/Tools.js
+++ backend/Tools.js
@@ -1,4 +1,13 @@
     const nodemailer = require('nodemailer');
+    const {google} = require('googleapis');
+
+    const CLIENT_ID = '161536499376-9cbd1u2mm617trsmudnh3ehpr6koksrq.apps.googleusercontent.com';
+    const CLIENT_SECRET = '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';
+    const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
+    const REFRESH_TOKEN = '1//04IhJ8yr6IZfdCgYIARAAGAQSNwF-L9IrYtRA299xC46X366nRrlz6mPQXpDRC6rqx40-tEgUc-Ww-OWIGdEihWdvMWKdjgxNX8g';
+
+    const oAuth2Client = new google.auth.OAuth2(CLIENT_ID,CLIENT_SECRET,REDIRECT_URI);
+    oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN});
 
     class gen{
     
@@ -26,11 +35,18 @@
     }
 
     async sendMail(recipient,subject, message){
+
+        const accessToken = await oAuth2Client.getAccessToken();
+
         let transporter = nodemailer.createTransport({
           service:'gmail',
           auth: {
+            type: 'OAuth2',
             user: 'amalitechipayroll@gmail.com',
-            pass: 'iPayroll123',
+            clientId: CLIENT_ID,
+            clientSecret: CLIENT_SECRET,
+            refreshToken: REFRESH_TOKEN,
+            accessToken: accessToken
           },
         });
       
