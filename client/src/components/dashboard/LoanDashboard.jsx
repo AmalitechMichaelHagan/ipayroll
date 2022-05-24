eb056b5d52b7e424c1a22fca8ecc8c78c868750d
__francis <francis.coffie@amalitech.org>
2022-05-24 15:17:04 +0000

    loan request

--- client/src/components/dashboard/LoanDashboard.jsx
+++ client/src/components/dashboard/LoanDashboard.jsx
@@ -8,3 +8,16 @@ import Footer from "../footer/Footer";
 export default function LoanDashboard() {
   const { register, handleSubmit } = useForm();
   const [data, setData] = useState("");
+
+  return (
+    <>
+      <Topbar />
+      <div className="">
+        <Sidebar />
+      </div>
+      <div className="background>">
+        <div className="flex2">
+          <div className="salary-set1">
+            <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+              <h4 className="loan">Loan request section</h4>
+              <hr className="line" />
