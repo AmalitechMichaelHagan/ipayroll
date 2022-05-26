ff0034d544e2041db6a9b5ae60b7c9b5ba3f3907
__francis <francis.coffie@amalitech.org>
2022-05-26 19:37:49 +0000

    forms

--- client/src/components/payslip/Payslip.jsx
+++ client/src/components/payslip/Payslip.jsx
@@ -2,9 +2,16 @@ import moment from "moment";
 import axios from "axios";
 
 
-export function LoginPage({ paySlipData }) {
+export function Payslip({ paySlipData }) {
     return (
       <section id="top">
          {paySlipData && 
            paySlipData.map((element, index) => {
-    
+          
+            return (
+                <div class="container mt-5 mb-5" key={index}>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="text-center lh-1 mb-2">
+                            <h6 class="fw-bold">Payslip</h6> <span class="fw-normal">Employee payslip for the month</span>
+                        </div>
