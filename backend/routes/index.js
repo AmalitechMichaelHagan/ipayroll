8e39c9e62a680e0071fc57b0112a6afa07008dc2
Michael Hagan <91923744+AmalitechMichaelHagan@users.noreply.github.com>
2022-05-30 06:00:04 +0000

    Report Route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const pool = require("../db");
+const tool = require("../Tools");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get("/report", async (req, res, next) => {
+  const {table} = req.body
+
+  try {
+      let data = await pool.query(`SELECT * FROM ${table}`);
+      data = data.rows;
+      let path = await tool.reportgen(data);
+      console.log(path);
+      res.json({
+        status: "success",
+        message: "file successfully downloaded",
+        path: path,
+        message:"Move the report from path before the next report generation to prevent the file from being overwritten."
+       });
+    
+  } catch (e) {
+      res.send(e.message)
+  }
+
+})
 
 module.exports = router;
