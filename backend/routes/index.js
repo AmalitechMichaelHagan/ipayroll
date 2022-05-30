440b6da3b7282a690a7c475aedc63e34ee7fe70f
Michael Hagan <91923744+AmalitechMichaelHagan@users.noreply.github.com>
2022-05-30 18:00:28 +0000

    report route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -9,24 +9,16 @@ router.get('/', function(req, res, next) {
 });
 
 router.get("/report", async (req, res, next) => {
-  const {table} = req.body
-
+  const {table} = req.body;
   try {
       let data = await pool.query(`SELECT * FROM ${table}`);
       data = data.rows;
       let path = await tool.reportgen(data);
-      console.log(path);
-      res.json({
-        status: "success",
-        message: "file successfully downloaded",
-        path: path,
-        message:"Move the report from path before the next report generation to prevent the file from being overwritten."
-       });
-    
+      console.log("success\n",path);
+      res.download(path);
   } catch (e) {
       res.send(e.message)
   }
-
 })
 
 module.exports = router;
