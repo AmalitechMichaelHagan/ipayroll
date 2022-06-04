689a6a011067b6c2f00f8ad59346c4a4cf58b821
Michael Hagan <91923744+AmalitechMichaelHagan@users.noreply.github.com>
2022-06-04 14:27:36 +0000

    sslChanges

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,8 @@ const pool = new Pool({
     password:"14ec022c01b999a5f15363cf63b18f3bb9180f48f1b5c7fa9aabd4cbd336380f",
     database:"d6q5704oqrvd16",
     host:"ec2-3-226-163-72.compute-1.amazonaws.com",
-    port:5432
+    port:5432,
+    ssl:{rejectUnauthorized: false}
 })
 
 module.exports = pool;
